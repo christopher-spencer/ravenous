31de0fa423da1cdad6ca0bcb878f81816254daa2
thebellywolf <cdspenzer@gmail.com>
2024-04-08 15:44:52 -0400

    updates to business details page

--- react/src/components/BusinessDetails/BusinessDetails.js
+++ react/src/components/BusinessDetails/BusinessDetails.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import styles from "./BusinessDetails.module.css";
 
 const BusinessDetails = ({ businesses }) => {
   // Get the business ID from the URL params
@@ -13,17 +15,25 @@ const BusinessDetails = ({ businesses }) => {
   }
 
   return (
-    <div>
-      <h2>{business.name}</h2>
-      <img src={business.imageSrc} alt={business.name} />
-      <p>Address: {business.address}</p>
-      <p>City: {business.city}</p>
-      <p>State: {business.state}</p>
-      <p>ZIP Code: {business.zipCode}</p>
-      <p>Category: {business.category}</p>
-      <p>Rating: {business.rating} stars</p>
-      <p>Review Count: {business.reviewCount} reviews</p>
-      {/* Add more details as needed */}
+    <div className={styles.businessDetailsContainer}>
+        <div className={styles.businessDetails}>
+        <h2>{business.name}</h2>
+        <div className={styles.imageContainer}>
+            <img src={business.imageSrc} alt={business.name} />
+        </div>
+        <p>Address: {business.address}</p>
+        <p>City: {business.city}</p>
+        <p>State: {business.state}</p>
+        <p>ZIP Code: {business.zipCode}</p>
+        <p>Category: {business.category}</p>
+        <p>Rating: {business.rating} stars</p>
+        <p>Review Count: {business.reviewCount} reviews</p>
+        </div>
+        <div className={styles.ReturnToBusinessesContainer}>
+            <Link to={"/"} className={styles.ReturnToBusinessesLink}>
+                Return to Businesses
+            </Link>
+        </div>
     </div>
   );
 };
